017ac942e128fff26ef91e5fd959588cdb03728c
Brent Rager <brent@rager.tech>
2025-03-09 12:10:17 -0500

    Fix typings and git hooks. (#29)

---
+++ .changeset/legal-eagles-wink.md
@@ -0,0 +1,5 @@
+---
+'@smooai/logger': patch
+---
+
+Add build to git hooks.
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, type Options } from 'tsup';
 export default defineConfig((options: Options) => ({
     entry: ['src/index.ts', 'src/env.ts', 'src/Logger.ts', 'src/BrowserLogger.ts', 'src/AwsLambdaLogger.ts', 'src/scripts/createEntryPoints.ts'],
     clean: true,
-    dts: false, // Turned off due to issues with @supabase/supabase-js/lib/types because of tyepof fetch and an error about it being private.
+    dts: true,
     format: ['cjs'],
     sourcemap: true,
     target: 'es2022',
