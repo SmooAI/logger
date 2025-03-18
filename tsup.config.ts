ac6a1fdc35c82385e9afe99821dc1c4397f6ff9e
Brent Rager <brent@rager.tech>
2025-03-18 19:47:58 -0400

    Update (#42)
    * Fix publish for Github releases.
    
    * Fix publish for Github releases.

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig((options: Options) => ({
     entry: ['src/index.ts', 'src/env.ts', 'src/Logger.ts', 'src/BrowserLogger.ts', 'src/AwsLambdaLogger.ts'],
     clean: true,
     dts: true,
-    format: ['cjs'],
+    format: ['cjs', 'esm'],
     sourcemap: true,
     target: 'es2022',
     treeShaking: true,
