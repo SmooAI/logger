46b8c912afc21038d2e5dd3c181ed8dcccb9b402
Brent Rager <brent@rager.tech>
2025-05-10 12:40:49 -0500

    Fix the BrowserLogger build and replace chalk with picocolors to fix issue with using BrowserLogger. (#66)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig, type Options } from 'tsup';
 
-export default defineConfig((options: Options) => ({
-    entry: ['src/index.ts', 'src/env.ts', 'src/Logger.ts', 'src/BrowserLogger.ts', 'src/AwsLambdaLogger.ts'],
+const coreConfig: Options = {
+    entry: ['src/index.ts', 'src/env.ts', 'src/Logger.ts', 'src/AwsLambdaLogger.ts'],
     clean: true,
     dts: true,
     format: ['cjs', 'esm'],
     sourcemap: true,
     target: 'es2022',
-    treeShaking: true,
-    ...options,
-}));
+};
+
+const browserConfig: Options = {
+    ...coreConfig,
+    entry: ['src/BrowserLogger.ts'],
+    platform: 'browser',
+};
+
+export default defineConfig([coreConfig, browserConfig]);
